838a3e46c5780deca664ef4917c423f5efbada0f
Dmitry <grammattika@gmail.com>
2023-05-21 19:57:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module_9_Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
