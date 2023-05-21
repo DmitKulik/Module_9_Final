129ff75e0c38a4387ee4b69f09af666de4b16ba8
Dmitry <grammattika@gmail.com>
2023-05-21 19:58:40 +0300

    first

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
-﻿namespace Module_9_Final
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿namespace Module_9_Final{
+    internal class Program{
+
+        static void Main(string[] args){
+
             Console.WriteLine("Hello, World!");
         }
     }
